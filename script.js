54a35358a3387406ac7e815fc01c6d6445e16bd7
JoelFasnacht <jfasnacht22@gmail.com>
2021-11-10 15:26:27 -0500

    paper function working

--- script.js
+++ script.js
@@ -3,31 +3,7 @@ let wins = 0;
 let draws = 0;
 let losses = 0;
 
-// function throwRock(){
 
-//   rnJesus = Math.ceil(Math.random()*5)
-
-//   if(response == 1){
-//     return "Draw!";
-//   }
-//   else if(response == 2){
-//     // scoreAI()
-//     return "Paper covers the Rock!"
-//   }
-//   else if(response == 3){
-//     // scoreYou()
-//     return "The Rock crushes scissors!"
-//   }
-//   else if(response == 4){
-//     // scoreYou()
-//     return "The Rock crushes lizard!"
-//   }
-//   else if(response == 5){
-//     // scoreAI()
-//     return "Spock vaporizes The Rock!"
-//   }
-  
-// }
 
 function throwPaper(){
 
@@ -61,76 +37,7 @@ else{
 
 }
 
-// winFunction(paper[rnJesus-1])
-
 }
 
-// function throwScissors(){
-//   rnJesus = Math.ceil(Math.random()*5)
 
-//   if(response == 1){
-//     return "The Rock crushes scissors!"
-//   }
-//   else if(response ==  2){
-//     return "Scissors cut paper!"
-//   }
-//   else if(response === 3){
-//     return "Draw!";
-//   }
-//   else if(response == 4){
-//     return "Scissors decapitate lizard!"
-//   }
-//   else if(response == 5){
-//     return "Spock smashes scissors!"
-//   }
-
-//}
-
-
-
-// function throwLizard(){
-//   rnJesus = Math.ceil(Math.random()*5)
-
-//   if(response == 1){
-//     return "The Rock crushes lizard";
-//   }
-//   else if(response == 2){
-//     return "Lizard eats paper!"
-//   }
-//   else if(response == 3){
-
-//     return "Scissors decapitate lizard!"
-//   }
-//   else if(response == 4){
-//     return "Draw!";
-//   }
-//   else if(response == 5){
-
-//     return "Lizard poisons Spock!"
-//   }
-
-// }
-
-// function throwSpock(){
-//   rnJesus = Math.ceil(Math.random()*5)
-
-//   if(response == 1){
-//     return "Spock vaporizes The Rock";
-//   }
-//   else if(response == 2){
-//     return "Paper disproves Spock!"
-//   }
-//   else if(response == 3){
-//     return "Spock smashes scissors!"
-//   }
-//   else if(response == 4){
-
-//     return "Lizard poisons Spock!"
-//   }
-//   else if(response == 5){
-
-//     return "Draw!";
-//   }
-
-// }
 
