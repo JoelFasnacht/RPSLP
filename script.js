960fa5ab4b8798e9ee9993bc0d5a7337600520d2
DonovanWinters <DonovaNewAge@gmail.com>
2021-11-10 16:28:18 -0500

    Playable Alpha

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let draws = 0;
 let losses = 0;
 
 
-
-function throwPaper(){
-
- let rnJesus = Math.ceil(Math.random()*5 - 1)
+const rock = 
+[["Draw!", "tie"],
+["Paper covers the Rock!", "lose"],
+["The Rock crushes scissors!", "win"],
+["The Rock crushes lizard!", "win"],
+["Spock vaporizes The Rock!", "lose"]];
 
 const paper =
 [["Paper covers The Rock!", "win"],
@@ -16,29 +18,27 @@ const paper =
 ["Lizard eats paper!", "lose"],
 ["Paper disproves Spock!", "win"]];
 
-console.log(rnJesus)
-console.log(paper[rnJesus])
-let result = paper[rnJesus]
-console.log(result[1])
-if(result[1] === "win"){
-  wins++
-  $("#win-score").text(wins)
-  $("#result").text(result[0])
-}
-else if(result[1] === "lose"){
-  losses++
-  $("#loss-score").text(losses)
-  $("#result").text(result[0])
-}
-else{
-  draws++
-  $("#draw-score").text(draws)
-  $("#result").text("Draw!")
+const scissors =
+[["The Rock crushes scissors!", "lose"],
+["Scissors cut paper!", "win"],
+["Draw!", "tie"],
+["Scissors decapitate lizard!", "win"],
+["Spock smashes scissors!", "lose"]];
 
-}
+const lizard = 
+[["The Rock crushes lizard!", "lose"],
+["Lizard eats paper!", "win"],
+["Scissors decapitate lizard!", "lose"],
+["Draw!", "tie"],
+["Lizard poisons Spock!", "win"]];
 
-}
 
+const spock = 
+[["Spock vaporizes The Rock", "win"],
+["Paper disproves Spock!", "lose"],
+["Spock smashes scissors!", "win"],
+["Lizard poisons Spock!", "lose"],
+["Draw!", "tie"]];
 
 function main(){
     
@@ -47,78 +47,30 @@ function main(){
 }
 
 
-function throwRock(){
+function throwX(playerChoice){
+  rnJesus = Math.ceil(Math.random()*5 -1)
 
-    rnJesus = Math.ceil(Math.random()*5 -1)
-    console.log(rnJesus);
-    const rock = 
-    [["Draw!", "tie"],
-    ["Paper covers the Rock!", "lose"],
-    ["The Rock crushes scissors!", "win"],
-    ["The Rock crushes lizard!", "win"],
-    ["Spock vaporizes The Rock!", "lose"]];
-    
-    console.log(rock[rnJesus])
+  $("#" + rnJesus).show(1000).delay(1000).hide(1000)
 
-    $("#" + rnJesus).show(1000).delay(1000).hide(1000)
-
+  winCondition(playerChoice[rnJesus])
 }
 
-function throwPaper(){
+function winCondition(result){
 
-    rnJesus = Math.ceil(Math.random()*5)
+  $("#result").text(result[0])
 
-    const paper =
-    [["Paper covers The Rock!", win],
-    ["Draw!", tie],
-    ["Scissors cuts paper!", lose],
-    ["Lizard eats paper!", lose],
-    ["Paper disproves Spock!", win]];
-
-    winFunction(paper[rnJesus-1])
-
-}
-
-function throwScissors(){
-
-    rnJesus = Math.ceil(Math.random()*5)
-
-    const scissors =
-    [["The Rock crushes scissors!", lose],
-    ["Scissors cut paper!", win],
-    ["Draw!", tie],
-    ["Scissors decapitate lizard!", win],
-    ["Spock smashes scissors!", lose]];
-
-    winFunction(scissors[rnJesus-1])
-
-}
-
-function throwLizard(){
-
-    rnJesus = Math.ceil(Math.random()*5)
-
-    const lizard = 
-    [["The Rock crushes lizard!", lose],
-    ["Lizard eats paper!", win],
-    ["Scissors decapitate lizard!", lose],
-    ["Draw!", tie],
-    ["Lizard poisons Spock!", win]];
-
-    winFunction(lizard[rnJesus-1])
-
-}
-
-function throwSpock(){
-
-    rnJesus = Math.ceil(Math.random()*5)
-
-    const spock = 
-    [["Spock vaporizes The Rock", win],
-    ["Paper disproves Spock!", lose],
-    ["Spock smashes scissors!", win],
-    ["Lizard poisons Spock!", lose],
-    ["Draw!", tie]];
-
-    winFunction(spock[rnJesus-1])
-
+  if(result[1] === "win"){
+    wins++
+    $("#win-score").text(wins)
+  }
+  else if(result[1] === "lose"){
+    losses++
+    $("#loss-score").text(losses)
+  }
+  else{
+    draws++
+    $("#draw-score").text(draws)
+    $("#result").text("Draw!")
+  
+  }
+ }
